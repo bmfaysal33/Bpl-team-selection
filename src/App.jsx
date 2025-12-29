ac1ad76fa44c183ee9e0ca9a3cd8db9283df577c
bmfaysal33 <bmfaysal33@gmail.com>
2025-12-29 08:34:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+import navImg from "./assets/logo.png";
+import dollarImg from "./assets/dollar-1.png";
+
+function App() {
+  return (
+    <>
+      <div className="navbar max-w-[1200px] mx-auto">
+        <div className="flex-1">
+          <a className=" text-xl">
+            <img className="w-[60px] h-[60px]" src={navImg} alt="" />
+          </a>
+        </div>
+        <div className="flex">
+          <span>6000000</span>
+          <span className="mx-1"> Coin</span>
+          <img src={dollarImg} alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
+export default App;
