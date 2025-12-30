d1752b841204e72855dc7256f96fd483af6d43d2
bmfaysal33 <bmfaysal33@gmail.com>
2025-12-30 22:53:41 +0600

    working with navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+
+
+
+<div className="navbar max-w-[1200px] mx-auto">
+        <div className="flex-1">
+          <a className=" text-xl">
+            <img className="w-[60px] h-[60px]" src={navImg} alt="" />
+          </a>
+        </div>
+        <div className="flex">
+          <span>6000000</span>
+          <span className="mx-1"> Coin</span>
+          <img src={dollarImg} alt="" />
+        </div>
+      </div>
